f8680d53b0e45abdce0d2a3417e6b252195e8346
umidovalimardon06 <umidovalimardon06@gmail.com>
2026-03-12 13:46:03 +0500

    add var,func hoisting

---
+++ variable-hoisting/let/let-hoisting.js
@@ -0,0 +1,13 @@
+console.log(a);
+let a = 100;
+console.log(a);
+
+/*console.log(a);
+let a;
+a = 100;
+console.log(a);*/
+
+/*let a;
+console.log(a);
+a = 100;
+console.log(a);*/
---
+++ variable-hoisting/var/var-hoisting.js
@@ -0,0 +1,13 @@
+console.log(a);
+var a = 100;
+console.log(a);
+
+/*console.log(a);
+var a;
+a = 100;
+console.log(a);*/
+
+/*var a;
+console.log(a);
+a = 100;
+console.log(a);*/
